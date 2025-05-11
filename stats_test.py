43ee7427be069c8eeb29b3fb9bc2b2f67cbe6404
giovanni.maffeo <giovanni.maffeo@gmail.com>
2025-05-11 10:58:21 +0100

    feat: stats for random_search vs custom_search

--- stats_test.py
+++ stats_test.py
@@ -59,19 +59,29 @@ def check_non_parametric_combinations_difference(evolve_type, algorithm, test_ty
   log_stats_msg(log_file, "Conclusion: No significant difference between combinations." if p > 0.05 else "Conclusion: At least one combination differs significantly from the others.")
   return p <= 0.05
 
-def check_pairwise_non_parametric_comparisons(evolve_type, algorithm, test_type, only_two):
-  reference_run_dir = f"outputs/{evolve_type}/{algorithm.__name__}/{test_type}/reference_run"
+def check_pairwise_non_parametric_comparisons(
+  evolve_type,
+  algorithm,
+  test_type,
+  only_two,
+  reference_run_dir=None,
+  combination_dirs=None
+):
+  if reference_run_dir is None:
+    reference_run_dir = f"outputs/{evolve_type}/{algorithm.__name__}/{test_type}/reference_run"
+
   log_file = os.path.join(reference_run_dir, "statistical_test_result.txt")
   step = "2" if only_two else "3"
   log_stats_msg(log_file, f"\n{step}. Pairwise Mann-Whitney U tests for combinations of algorithm '{algorithm.__name__}' and test '{test_type}':")
 
-  combination_dirs = sorted([
-    os.path.join(reference_run_dir, d)
-    for d in os.listdir(reference_run_dir)
-    if d.startswith("combination")
-  ])
-  all_combination_fitnesses = []
+  if combination_dirs is None:
+    combination_dirs = sorted([
+      os.path.join(reference_run_dir, d)
+      for d in os.listdir(reference_run_dir)
+      if d.startswith("combination")
+    ])
 
+  all_combination_fitnesses = []
   for combination_dir in combination_dirs:
     combination_fitness_values = []
     for run_index in range(1, 6):
@@ -133,19 +143,29 @@ def check_pairwise_non_parametric_comparisons(evolve_type, algorithm, test_type,
   for rank, (idx, win_count) in enumerate(ranked_corrected, start=1):
     log_stats_msg(log_file, f"{rank}. Combination {idx+1} — {win_count} wins")
 
-def check_pairwise_parametric_comparisons(evolve_type, algorithm, test_type, only_two):
-  reference_run_dir = f"outputs/{evolve_type}/{algorithm.__name__}/{test_type}/reference_run"
+def check_pairwise_parametric_comparisons(
+  evolve_type,
+  algorithm,
+  test_type,
+  only_two,
+  reference_run_dir=None,
+  combination_dirs=None
+):
+  if reference_run_dir is None:
+    reference_run_dir = f"outputs/{evolve_type}/{algorithm.__name__}/{test_type}/reference_run"
+
   log_file = os.path.join(reference_run_dir, "statistical_test_result.txt")
   step = "2" if only_two else "3"
   log_stats_msg(log_file, f"\n{step}. Pairwise Independent t-tests for combinations of algorithm '{algorithm.__name__}' and test '{test_type}':")
 
-  combination_dirs = sorted([
-    os.path.join(reference_run_dir, d)
-    for d in os.listdir(reference_run_dir)
-    if d.startswith("combination")
-  ])
-  all_combination_fitnesses = []
+  if combination_dirs is None:
+    combination_dirs = sorted([
+      os.path.join(reference_run_dir, d)
+      for d in os.listdir(reference_run_dir)
+      if d.startswith("combination")
+    ])
 
+  all_combination_fitnesses = []
   for combination_dir in combination_dirs:
     combination_fitness_values = []
     for run_index in range(1, 6): 
--- stats_test_evolve_structure.py
+++ stats_test_evolve_structure.py
@@ -1,6 +1,6 @@
 from ea_structure import ea_search
 from random_structure import random_search
-from stats_test import base_stat_test
+from stats_test import base_stat_test, check_pairwise_non_parametric_comparisons
 import utils
 
 evolve_type = utils.evolve_types["evolve_structure"]["label"]
@@ -19,6 +19,19 @@ def stats_ea_search_controller_scenario_test():
   algorithm = ea_search
   test_type = utils.test_types[1]
   base_stat_test(evolve_type, algorithm, test_type)
+
+def stats_random_search_vs_ea_search_controller_scenario_test():
+  check_pairwise_non_parametric_comparisons(
+  evolve_type="-",
+  algorithm="-",
+  test_type="random_search_vs_ea_search_test",
+  only_two=True,
+  combination_dirs=[
+    "outputs/evolve_structure/random_search/controller_scenario_test/reference_run/combination6",
+    "outputs/evolve_structure/ea_search/controller_scenario_test/reference_run/combination6"
+  ],
+  reference_run_dir="outputs/evolve_structure"
+)
 
 # stats_ea_search_hiperparams_fatorial_test()
 # stats_random_search_controller_scenario_test()
